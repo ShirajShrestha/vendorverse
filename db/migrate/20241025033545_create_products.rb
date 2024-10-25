49502292d753a5b6869478844ff5e0fe7fd5191a
ShirajShrestha <shresthashiraj333@gmail.com>
2024-10-25 07:48:29 +0000

    Add devise and create product model and controller

---
+++ db/migrate/20241025033545_create_products.rb
@@ -0,0 +1,15 @@
+class CreateProducts < ActiveRecord::Migration[7.2]
+  def change
+    create_table :products do |t|
+      t.string :name
+      t.text :description
+      t.string :image
+      t.decimal :price
+      t.boolean :sold, default: false
+      t.string :category
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
